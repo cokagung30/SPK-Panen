c354acacd83d6fab08d55034bc31f8a9ac508116
cokagun09 <cokgung09@gmail.com>
2019-06-27 22:56:38 +0800

    Pembaharuan edit data ayam

--- application/controllers/pegawai/DataAyam.php
+++ application/controllers/pegawai/DataAyam.php
@@ -7,7 +7,8 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class DataAyam extends CI_Controller{
+class DataAyam extends CI_Controller
+{
 
     public function __construct()
     {
@@ -15,41 +16,101 @@ class DataAyam extends CI_Controller{
         $this->load->model('MDataAyam', 'dataAyam');
     }
 
-    public function updateDataAyam($id_data_ayam){
-//        $id_periode = $this->session->userdata('id_periode');
-//        $umur = $this->input->post('umur');
-//        $tanggal = $this->input->post('tanggal');
-//        $ip = $this->input->post('ip');
-//        $fcr = $this->input->post('fcr');
-        $jml_mati_sebelumnya = $this->input->post('jm_mati_sebelumnya');
+    public function updateDataAyam($id_data_ayam)
+    {
+
+        $id_periode = $this->session->userdata('id_periode');
+        $umur = $this->input->post('umur');
+        $tanggal = $this->input->post('tanggal');
         $jml_mati = $this->input->post('jumlahmati');
-        $mortalitas = $this->input->post('mortalitas');
         $berat_rata = $this->input->post('beratrata');
-        $berat_sebelumnya = $this->input->post('berat_rata_sebelumnya');
+        $jml_pakan = $this->input->post('jumlahpakan');
+        $harga = $this->input->post('hargajual');
 
-        $mortalitasUpdate = $this->mortalitas($mortalitas, $jml_mati, $jml_mati_sebelumnya);
-        echo "".$mortalitasUpdate;
-    }
+        $jml_mati_sebelumnya = $this->input->post('jm_mati_sebelumnya');
+        $jml_pakan_sebelumnya = $this->input->post('jml_pakan_sebelumnya');
+        $berat_rata_sebelumnya = $this->input->post('berat_rata_sebelumnya');
 
-    public function mortalitas($mortalitas, $jml_mati, $jml_mati_sebelumnya){
-        $hasilMortalitas = 0;
-        $volume = $this->session->userdata('volume');
-
-        if ($jml_mati > $jml_mati_sebelumnya){
-            $jml_mati_update = $jml_mati - $jml_mati_sebelumnya;
-            $mortalitas1 = ($jml_mati_update/$volume)*100;
-            $hasilMortalitas = $mortalitas + $mortalitas1;
-        } else if ($jml_mati < $jml_mati_sebelumnya){
-            $jml_mati_update = $jml_mati_sebelumnya - $jml_mati;
-            $mortalitas1 = ($jml_mati_update/$volume)*100;
-            $hasilMortalitas = $mortalitas - $mortalitas1;
+        if ($berat_rata != $berat_rata_sebelumnya) {
+            $fcr = $this->updateFcr($id_data_ayam, $berat_rata);
         } else {
-            $hasilMortalitas = $mortalitas;
+            $fcr = $this->input->post('fcr');
         }
 
+        $ip = $this->hasilIpUpdate($berat_rata, $umur, $id_data_ayam);
 
+        $data = array(
+            'umur' => $umur,
+            'tanggal' => $tanggal,
+            'jml_mati' => $jml_mati,
+            'jml_pakan' => $jml_pakan,
+            'berat_rata' => $berat_rata,
+            'fcr' => round($fcr, 3),
+            'ip' => round($ip, 2),
+            'harga' => $harga
+        );
 
-        return $hasilMortalitas;
+        $update = $this->dataAyam->updateDataAyam($id_data_ayam, $id_periode, $data);
+
+        if ($update > 0) {
+            $selisihMortalitas = $this->selisihUntukMortalitas($jml_mati, $jml_mati_sebelumnya);
+            $selisihFcr = $this->selisihUntukFcr($jml_pakan, $jml_pakan_sebelumnya, $berat_rata);
+            $updateData = round($this->dataAyam->updateDataMengurangi($selisihFcr, $selisihMortalitas, $id_data_ayam), 3);
+            if ($updateData) {
+                redirect(base_url() . "pegawai/Data_ayam/index");
+            } else {
+                redirect(base_url() . "pegawai/Data_ayam/index");
+            }
+        } else {
+            redirect(base_url() . "pegawai/Data_ayam/index");
+        }
+
+    }
+
+    public function selisihUntukMortalitas($jml_mati, $jml_mati_sebellumnya)
+    {
+        $selisihJmlMati = $jml_mati - $jml_mati_sebellumnya;
+        $volume = $this->session->userdata('volume');
+        $mortalitas = ($selisihJmlMati / $volume) * 100;
+
+        return $mortalitas;
+    }
+
+    public function selisihUntukFcr($jml_pakan, $jml_pakan_sebelumnya, $berat_rata)
+    {
+        $selisihJmlPakan = $jml_pakan - $jml_pakan_sebelumnya;
+        $fcr = ($selisihJmlPakan / $berat_rata) * 10;
+
+        return $fcr;
+    }
+
+    public function updateFcr($id_data_ayam, $berat_rata)
+    {
+        $select = $this->dataAyam->getJmlMaxJmlPakan($id_data_ayam, $this->session->userdata('id_periode'));
+        foreach ($select->result() as $item) {
+            $jml_pakan = $item->jml_pakan;
+        }
+        $fcr = ($jml_pakan / $berat_rata) * 10;
+        return $fcr;
+    }
+
+    public function hasilIpUpdate($berat_rata, $umur, $id_data_ayam)
+    {
+        $id_periode = $this->session->userdata('id_periode');
+        $volume = $this->session->userdata('volume');
+
+        $select = $this->dataAyam->getJmlMaxJmlPakan($id_data_ayam, $id_periode);
+        foreach ($select->result() as $item) {
+
+            $fcr = ($item->jml_pakan / $berat_rata)*10;
+
+            $mati = $item->jml_mati;
+            $ayam_hidup = $volume - $mati;
+
+            $ip = ((($ayam_hidup/$volume)*$berat_rata)/($umur *$fcr))*10;
+        }
+
+        return $ip;
     }
 
 }
